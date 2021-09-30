c0fcefe0d1ac635bac7f122b2571b6f71e6470b5
junky <50148641+junkypurr@users.noreply.github.com>
2021-09-30 02:54:41 +0000

    changelog, woo
    - edited ".replit" folder
    - edited "README"
    - edited "third.py" so that there's actually code in it (crazy i know)
    - added ".breakpoints" file
    - added "poetry.lock" file
    - added "pyproject.toml" file
    - added "fourth.py" and threw some code into there
    - added "fifth.py" and told it to do something
    - added "sixth.py" and then threw code into it, but then it didn't save.

---
+++ python/fifth.py
@@ -0,0 +1,11 @@
+animal = input("Enter an animal: ")
+sound = input ("Enter a sound: ")
+
+jingle = "E-I-E-I-O"
+
+print ("Old Macdonald had a farm, " + jingle)
+print ("And on his farm he had a " + animal + ", " + jingle)
+print ("With a " + sound + "-" + sound + " here and a " + sound + "-" + sound + " there")
+print ("Here a "+ sound+ " there a " +sound)
+print ("Everywhere a " + sound + "-" + sound )
+print ("Old Macdonald had a farm, " + jingle)
---
+++ python/third.py
@@ -0,0 +1,5 @@
+num = int(input ("Enter a number: "))
+print (num * 8)
+
+
+# int() tells python that youre asking for an integer value, input tells it that youre asking for user input. it then prints the users input number and then multiplies it by 8. 
